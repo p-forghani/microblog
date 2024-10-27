557770299a0a4bda5731155927024f348c29fec3
pouria <forghani.dev@gmail.com>
2024-10-27 13:12:05 +0330

    Add user registeration and profile page

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,52 @@
+import sqlalchemy as sa
+import re
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from app import db
+from app.models import User
+from app.constants import MIN_PASSWORD_LENGTH
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
+
+    # When you add any methods that match the pattern validate_<field_name>,
+    # WTForms takes those as custom validators and invokes them in addition
+    # to the stock validators.
+    def validate_username(self, username):
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username.data)
+        )
+        if user is not None:
+            raise ValidationError("Username already exists")
+
+    def validate_email(self, email):
+        user = db.session.scalar(
+            sa.select(User).where(User.email == email.data)
+        )
+        if user is not None:
+            raise ValidationError("Email already exists")
+
+    def validate_password(self, password):
+        if len(password.data) < MIN_PASSWORD_LENGTH:
+            raise ValidationError(f"Password must be at least \
+                                  {MIN_PASSWORD_LENGTH} characters long")
+        if not re.search(r"[A-Za-z]", password.data):
+            raise ValidationError(
+                "Password must contain as least 1 letter"
+            )
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask import flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -28,29 +28,38 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     # Checks if the user is already logged-in
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+
     form = LoginForm()
+
     # Check if form is submitted with data and passes the validations
     if form.validate_on_submit():
+
         # Executes the query and returns a single scalar value
         user = db.session.scalar(
             sa.select(User).where(User.username == form.username.data)
         )
+
         # Checks if user exists and password is correct
         if (user is None) or (not user.check_password(form.password.data)):
             flash("Invalid username or password")
             return redirect(url_for('login'))
+
         # Registers the user as logged in
         login_user(user, remember=form.remember_me.data)
+
         next_page = request.args.get('next')
+
         # netloc is the network location (e.g., example.com, localhost:8000),
         # if netloc is not empty it means url contains domain
         # Full url including the domain name is ignored due to the security
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
+
     # Shows login page
     return render_template('login.html', title='Sign In', form=form)
 
@@ -59,3 +68,29 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("You are now registered")
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"},
+    ]
+    return render_template('user.html', user=user, posts=posts)
